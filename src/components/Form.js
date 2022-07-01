717dfa823e254aa6e2323c6dc333901fd45f6699
kisnj <knandor407@gmail.com>
2022-07-01 10:20:23 +0200

    creating form field

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,40 @@
-import React from 'react'
-export default function Form(){
-    return(
-        <div>
-            
-        </div>
-    )
+import React, { useState } from "react";
+import AddEducation from "./AddEducation";
+import AddExperience from "./AddExperience";
+export default function Form() {
+  const [formData, setFormData] = useState({
+    name: "",
+    job_title: "",
+    exerience: [],
+    education: [],
+    contanct: [],
+    languages: [],
+    technologies: [],
+  });
+  function addExp(e) {
+    e.preventDefault();
+  }
+  function addEdu(e) {
+    e.preventDefault();
+  }
+  return (
+    <form>
+      <fieldset>
+        <label htmlFor="name">Enter your name</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="job_title">Enter your job title</label>
+        <input type="text" name="job_title" id="job_title" />
+      </fieldset>
+      <fieldset>
+        <legend>EXPERIENCE</legend>
+        <AddExperience />
+        <button onClick={addExp}>Add Exerience</button>
+      </fieldset>
+      <fieldset>
+        <legend>EDUCATION</legend>,
+        <AddEducation />
+        <button onClick={addEdu}>Add Education</button>
+      </fieldset>
+    </form>
+  );
 }
